3feacc63e6ab002457d8a217c11739d00ea32b1c
Patrick Riggs <riggspc@gmail.com>
2014-01-23 22:23:46 -0500

    Got behavior working, added screenshots

--- src/com/example/riggspc_helloworld/MainActivity.java
+++ src/com/example/riggspc_helloworld/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.riggspc_helloworld;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "com.example.riggspc_helloworld.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,16 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    	
+    	
     }
     
 }
